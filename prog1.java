47ab2912ade355a8a65370dcb314cb47f330f8e4
dpknegi <deepak@zemosolabs.com>
2014-02-21 17:36:56 +0530

    Create prog1.java

---
+++ prog1.java
@@ -0,0 +1,14 @@
+public class prog1{
+  public class void main(String []args){
+   int i;
+   
+   for (i=0;i<100;i++){
+   System.Out.println("The count is at "+  i);
+   }
+  
+  
+  
+  
+  }
+  
+}
