73050af71d2951ca6a566a6e344d14a29c281e1b
dpknegi <deepak@zemosolabs.com>
2014-02-25 10:02:15 +0530

    Go commit

---
+++ Euler1.java
@@ -0,0 +1,44 @@
+public class Euler1{
+	public static void main (String []args){
+		// default arguments
+		int factor_1=3;
+		int factor_2=5;
+		int limit=1000;
+		
+		// if arguments are supplied
+		if (args.length>0){
+				factor_1=Integer.parseInt(args[0]);
+				factor_2=Integer.parseInt(args[1]);
+				limit=Integer.parseInt(args[2]);
+		}
+		
+		int sum_factor_1,sum_factor_2,sum_lcm,result;
+		int temp,count_factor_1,count_factor_2,count_lcm,lcm,hcf,hcf_variable;
+		count_factor_1= limit/factor_1;
+		count_factor_2= limit/factor_2;
+		//finding lcm
+		if (factor_1 >factor_2){
+			hcf=factor_2;
+			hcf_variable=factor_1;
+		}
+		else{
+			hcf=factor_1;
+			hcf_variable=factor_2;
+		}
+		while(hcf_variable%hcf != 0){
+			temp=hcf;
+			hcf=hcf_variable%hcf;
+			hcf_variable=temp;
+		}
+			
+		lcm=(factor_1*factor_2)/hcf;
+		count_lcm=limit/lcm;
+		//calculating sum
+		sum_factor_1=(count_factor_1* (( 2*count_factor_1)+((count_factor_1-1)*count_factor_1))/2;
+		sum_factor_2=(count_factor_2* (( 2*count_factor_2)+((count_factor_2-1)*count_factor_2))/2;
+		sum_lcm=(count_lcm* (( 2*count_lcm)+((count_lcm-1)*count_lcm))/2;
+		result=sum_factor_1+sum_factor_2-sum_lcm;
+
+		System.out.println("The sum of multiples of "+ factor_1 +" and "+factor_2 +" is "+ sum);
+	}
+}
--- Euler4.java
+++ Euler4.java
@@ -1,46 +1,25 @@
 public class Euler4{
-	public static void main(String args[]){
-		int product,number1,number2,max_product;
-		max_product=1;
-		//the product has to be a 6 digit number
-		for(number1=999;number1>=100;number1--){
-			for(number2=999; number2>=100;number2--){
-				product=number1*number2;
-				if (check_palindrome(product)==1){
-					if (product>max_product){
-						max_product=product;
-					}
-				}
+	public static void main(String[] args){
+		int solution = createPalindrome();
+		System.out.println("The desired palindrome is " + solution);
+
+	}
+
+	private static int createPalindrome(){
+		int product = 0;
+		int max=1;
+		for (int i = 999; i >= 900; i--)
+		{
+			for (int j =i; j >= 900; j--)
+			{
+				product = i * j;
+				String s1 = Integer.toString(product);
+				String s2 = new StringBuffer(s1).reverse().toString();
+				if (s1.equals(s2))
+					if (product>max)
+						max=product;
 			}
 		}
-		System.out.println("The largest palindrome which is a product of two 3-digit numbers is "+ max_product);
-
+		return max;
 	}
-
-
-	private static int check_palindrome(int number){
-		int array[]= new int[5]; 
-		int length=0;
-		int original=number;
-		while(number/10 != 0){
-		 	length++;
-		 	number/=10;
-		}
-		number=original;
-		//getting the digits in the number
-		for(int i=0;i<length;i++){
-			array[i]=number%10;
-			number /=10;
-		}
-		System.out.println("The largest palindrome which is a product of two 3-digit numbers is "+ array[0]);
-
-		// checking palindrome
-		for(int i=0;i<length/2;i++){
-			if (array[i]!= array[length-i]){
-				return 0;
-			}		
-		}
-		return 1;
-	}
-
 }
